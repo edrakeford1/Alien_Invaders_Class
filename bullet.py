1ed19aa749b0582a157e2e0359093743e803c2c9
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-04-07 15:18:44 -0400

    bullets working

--- bullet.py
+++ bullet.py
@@ -8,8 +8,8 @@ class Bullet(Sprite):
         """ Create a bullet object at the ship's current position """
         super().__init__()
         self.screen = ai_game.screen
-        self.settings - ai_game.settings
-        self.color - self.settings.bullet_color
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0, 0) and then set the correct position
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
@@ -28,4 +28,3 @@ class Bullet(Sprite):
     def draw_bullet(self):
         """ Draw the bullet to the screen """
         pygame.draw.rect(self.screen, self.color, self.rect)
-        
